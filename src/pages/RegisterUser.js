ee95df2d3371f9f68f7e90af4346aff035d60f42
Jéssica Caroline Pires Cabral <Jessica53793446@edu.df.senac.br>
2025-07-24 20:51:05 -0300

    API: funcao inserir autor

--- src/pages/RegisterUser.js
+++ src/pages/RegisterUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   View,
   ScrollView,
@@ -8,9 +8,27 @@ import {
 } from "react-native";
 import Mytextinput from "./components/Mytextinput";
 import Mybutton from "./components/Mybutton";
+import axios from 'axios';
+
 
 const RegisterUser = ({ navigation }) => {
-  const [nome, setNome] = useState('');
+  const [nomeAutor, setNomeAutor] = useState('');
+
+  //inserir autor
+  async function inserirAutor () {
+    try {
+      //informa a url da api e os parametros
+      const resposta = await axios.post('http://10.0.2.2:81/livros_php/api.php', {
+        nome: nomeAutor,
+      });
+      navigation.navigate('HomeScreen');
+      alert ("Autor inserido com sucesso!");
+    } catch (error) {
+      navigation.navigate('HomeScreen');
+      alert ("Erro ao inserir autor");
+    }
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, backgroundColor: "white" }}>
@@ -23,9 +41,11 @@ const RegisterUser = ({ navigation }) => {
               <Mytextinput
                 placeholder="Nome do autor"
                 style={{ padding: 10 }}
+                value={nomeAutor}
+                onChangeText={setNomeAutor}
               />
 
-              <Mybutton title="Salvar" />
+              <Mybutton title="Salvar" onPress={inserirAutor}/>
             </KeyboardAvoidingView>
           </ScrollView>
         </View>
